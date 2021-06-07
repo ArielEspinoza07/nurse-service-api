8699a0ca8c477795dbc641cbb164657500299c80
ArielEspinoza07 <arielespinoza46@gmail.com>
2021-06-07 15:31:36 -0600

    add IsAdminCriteria to filter results

---
+++ app/Criteria/IsAdminCriteria.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Criteria;
+
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class IsAdminCriteria.
+ *
+ * @package namespace App\Criteria;
+ */
+class IsAdminCriteria implements CriteriaInterface
+{
+
+    /**
+     * Apply criteria in query repository
+     *
+     * @param string              $model
+     * @param RepositoryInterface $repository
+     *
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        if (auth()
+            ->user()
+            ->hasRole('admin')) {
+
+            return $model;
+        }
+
+        return $model->where('user_id', auth()->id());
+    }
+}
